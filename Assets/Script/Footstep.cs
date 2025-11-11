1f350fa2e9828e4f53e0d92998c513f91c8a1dfb
Bruno Chen <998isyi@gmail.com>
2025-11-11 14:56:17 -0300

    walking, running , crouching sound

--- Assets/Script/Footstep.cs
+++ Assets/Script/Footstep.cs
@@ -1,19 +1,78 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Footstep : MonoBehaviour
+public class PlayerFootsteps : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public AudioSource footstepsSound;
-    
-    // Update is called once per frame
+    [Header("AudioSources (arraste 4)")]
+    public AudioSource footstepsNormal;
+    public AudioSource footstepsRun;
+    public AudioSource footstepsCrouch;
+    public AudioSource jumpSound;
+
+    [Header("Teclas")]
+    public KeyCode runKey = KeyCode.LeftShift;
+    public KeyCode crouchKey = KeyCode.LeftControl;
+    public KeyCode jumpKey = KeyCode.Space;
+
     void Update()
     {
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)){
-            footstepsSound.enabled = true;
+        // Movimento
+        bool moving = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A)
+                    || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
+
+        bool running = Input.GetKey(runKey);
+        bool crouching = Input.GetKey(crouchKey);
+
+        // --- Lógica de passos ---
+        if (moving)
+        {
+            if (running)
+            {
+                PlayOnly(footstepsRun);
+            }
+            else if (crouching)
+            {
+                PlayOnly(footstepsCrouch);
+            }
+            else
+            {
+                PlayOnly(footstepsNormal);
+            }
         }
         else
         {
-            footstepsSound.enabled = false;
+            StopAll();
         }
+
+        // --- Som de pulo ---
+        if (Input.GetKeyDown(jumpKey))
+        {
+            if (jumpSound != null)
+                jumpSound.PlayOneShot(jumpSound.clip);
+        }
+    }
+
+    // 🔊 Toca só o som escolhido e para os outros
+    void PlayOnly(AudioSource source)
+    {
+        if (source == null) return;
+
+        if (!source.isPlaying)
+            source.Play();
+
+        // parar os outros
+        if (source != footstepsNormal && footstepsNormal.isPlaying)
+            footstepsNormal.Stop();
+        if (source != footstepsRun && footstepsRun.isPlaying)
+            footstepsRun.Stop();
+        if (source != footstepsCrouch && footstepsCrouch.isPlaying)
+            footstepsCrouch.Stop();
+    }
+
+    // 🔇 Para todos
+    void StopAll()
+    {
+        if (footstepsNormal.isPlaying) footstepsNormal.Stop();
+        if (footstepsRun.isPlaying) footstepsRun.Stop();
+        if (footstepsCrouch.isPlaying) footstepsCrouch.Stop();
     }
 }
